20ea728aeeded8b64f81e6378c5ff3ac424201fd
RodResq <rodrigoresque@yahoo.com.br>
2025-09-13 09:27:10 -0300

    feat: add component navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CytoscapeComponent } from './cytoscape/cytoscape.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CytoscapeComponent],
+  imports: [CytoscapeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
